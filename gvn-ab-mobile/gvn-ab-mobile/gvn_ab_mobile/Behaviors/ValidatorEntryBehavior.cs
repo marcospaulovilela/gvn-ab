e958a4db5d40facaff04049430e3ed66900e3e4c
marcospaulovilela <marcos_paulo_vilela@hotmail.com>
2017-12-17 20:51:38 -0200

    Implementada função de validação do C.P.F.

---
+++ gvn-ab-mobile/gvn-ab-mobile/gvn_ab_mobile/Behaviors/ValidatorEntryBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace gvn_ab_mobile.Behaviors {
+    public abstract class ValidatorEntryBehavior : Behavior<Entry> {
+        private Color DefaultColor { get; set; }
+
+        protected int? MaxLength { get; set; }
+
+        protected override void OnAttachedTo(Entry bindable) {
+            this.DefaultColor = bindable.TextColor;
+            bindable.TextChanged += onTextChanged;
+            
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(Entry bindable) {
+            bindable.TextChanged -= onTextChanged;
+            base.OnDetachingFrom(bindable);
+        }
+
+        /// <summary>
+        /// Responsavel por validar a entrada no componente Entry.
+        /// </summary>
+        /// <param name="input">Entrada do Entry.</param>
+        /// <returns> TRUE se a validação tiver sucesso, FALSE se não.</returns>
+        public abstract bool Validate(string input);
+
+        void onTextChanged(object sender, TextChangedEventArgs args) {
+            var entry = (Entry)sender;
+
+            //MUDA A COR CASO TENHA ERRO.
+            entry.TextColor = (this.Validate(args.NewTextValue)) ? this.DefaultColor : Color.DarkOrange;
+
+            //LIMITA O TAMANHO DO TEXTO
+            if (entry.Text.Length > MaxLength) {
+                entry.Text = args.OldTextValue;
+            };
+        }
+    }
+}
---
+++ gvn-ab-mobile/gvn-ab-mobile/gvn_ab_mobile/Behaviors/Validators.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gvn_ab_mobile.Behaviors {
+    public class ValidatorsCpf : ValidatorEntryBehavior {
+        
+        public ValidatorsCpf() {
+            base.MaxLength = 11;
+        }
+
+        private bool IsCpf(string cpf) {
+            string digits = "";
+
+            int sum = 0, aux = 0;
+            for(int i = 2; i <= 10; i++) {
+                sum += int.Parse(cpf[8 - (i - 2)].ToString()) * i;
+            };
+
+            digits = ((aux = sum % 11) < 2) ? "0" : (11 - aux).ToString();
+
+            sum = 0;
+            for (int i = 3; i <= 11; i++) {
+                sum += int.Parse(cpf[8 - (i - 3)].ToString()) * i;
+            };
+            sum += int.Parse(digits) * 2;
+
+            digits += ((aux = sum % 11) < 2) ? "0" : (11 - aux).ToString();
+
+            return cpf.EndsWith(digits);
+        }
+
+        public override bool Validate(string input) {
+            input = input.Replace(".", "").Replace(",", "").Replace("-", "");
+
+            if (input.Length != 11) return false;
+            return IsCpf(input);
+        }
+    }
+}
