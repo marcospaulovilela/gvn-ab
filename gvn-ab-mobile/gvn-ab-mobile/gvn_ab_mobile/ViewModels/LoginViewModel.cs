a1530d41ed630ec0f90ccc9251ba574fa68731c5
Matheus Prandini <matheusprandini.96@gmail.com>
2017-12-19 21:01:42 -0200

    Criação DAO Fichas

--- gvn-ab-mobile/gvn-ab-mobile/gvn_ab_mobile/ViewModels/LoginViewModel.cs
+++ gvn-ab-mobile/gvn-ab-mobile/gvn_ab_mobile/ViewModels/LoginViewModel.cs
@@ -7,11 +7,11 @@ namespace gvn_ab_mobile.ViewModels {
         private Page Page { get; set; }
         public ICommand Login{ get; private set; }
         
-        public Models.Usuario usuario {get;set;}
+        public Models.Usuario Usuario {get;set;}
         
 
         public LoginViewModel(Page page) {
-            this.usuario = new Models.Usuario();
+            this.Usuario = new Models.Usuario();
             this.Page = page;
 
             this.Login = new Command(async () => await LoginExecuteAsync());
@@ -19,9 +19,9 @@ namespace gvn_ab_mobile.ViewModels {
 
         private async System.Threading.Tasks.Task LoginExecuteAsync() {
             using (var objDao = new DAO.DAOUsuario()) {
-                var CpfUser = objDao.GetUsuarioByCpf(this.usuario.Cpf);
-                if(CpfUser != null && CpfUser.Password == this.usuario.Password) { //SERIO??? BRINCADEIRA SEGURANÇA FAZER DIREITO DEPOIS.....
-                    this.usuario.Cpf = this.usuario.Password = ""; //limpa a entidade para caso voltar no login.
+                var CpfUser = objDao.GetUsuarioByCpf(this.Usuario.Cpf);
+                if(CpfUser != null && CpfUser.Password == this.Usuario.Password) { //SERIO??? BRINCADEIRA SEGURANÇA FAZER DIREITO DEPOIS.....
+                    this.Usuario.Cpf = this.Usuario.Password = ""; //limpa a entidade para caso voltar no login.
                     await this.Page.Navigation.PushAsync(new Views.MenuPage());
                 } else {
                     await this.Page.DisplayAlert("Erro de login", "Usuario ou senha invalidos", "Ok");
