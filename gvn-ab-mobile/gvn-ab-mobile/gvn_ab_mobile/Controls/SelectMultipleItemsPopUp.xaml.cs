b99ed27bbbd52a3508170e57913da4ca9a2abd51
marcospaulovilela <marcos_paulo_vilela@hotmail.com>
2018-01-17 00:54:05 -0200

    SearcherPicker

--- gvn-ab-mobile/gvn-ab-mobile/gvn_ab_mobile/Controls/SelectMultipleItemsPopUp.xaml.cs
+++ gvn-ab-mobile/gvn-ab-mobile/gvn_ab_mobile/Controls/SelectMultipleItemsPopUp.xaml.cs
@@ -14,6 +14,7 @@ namespace gvn_ab_mobile.Controls {
             InitializeComponent();
 
             this.BindingContext = viewModel;
+            this.CloseWhenBackgroundIsClicked = false;
         }
 
         private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e) {
