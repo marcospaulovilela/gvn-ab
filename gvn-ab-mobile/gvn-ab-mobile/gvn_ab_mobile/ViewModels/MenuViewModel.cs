a8399d71d69b282dde37061733857cc409015460
Marcos Paulo Vilela <marcos_paulo_vilela@hotmail.com>
2018-01-30 09:10:45 -0200

    Update MenuViewModel.cs

--- gvn-ab-mobile/gvn-ab-mobile/gvn_ab_mobile/ViewModels/MenuViewModel.cs
+++ gvn-ab-mobile/gvn-ab-mobile/gvn_ab_mobile/ViewModels/MenuViewModel.cs
@@ -23,21 +23,21 @@ namespace gvn_ab_mobile.ViewModels {
         public bool CboCadastroIndividual {
             get {
                 string[] CbosAltorizados = new string[] { "322205", "322210", "322230", "322245", "322250", "322405", "322415", "322425", "322430", "352210", "515105", "515120", "515125", "515130", "515140", "515305", "515310", "422110" };
-                return true || CbosAltorizados.Any(o => o.Equals(this.Cbo.CodCbo));
+                return CbosAltorizados.Any(o => o.Equals(this.Cbo.CodCbo));
             }
         }
 
         public bool CboCadastroDomiciliar {
             get {
                 string[] CbosAltorizados = new string[] { "322205", "322210", "322230", "322245", "322250", "322405", "322415", "322425", "322430", "352210", "515105", "515120", "515125", "515130", "515140", "515305", "515310", "422110" };
-                return true || CbosAltorizados.Any(o => o.Equals(this.Cbo.CodCbo));
+                return CbosAltorizados.Any(o => o.Equals(this.Cbo.CodCbo));
             }
         }
 
         public bool CboVisitaDomiciliar {
             get {
                 string[] CbosAltorizados = new string[] { "515105", "515120", "515310", "515140" };
-                return true || CbosAltorizados.Any(o => o.Equals(this.Cbo.CodCbo));
+                return CbosAltorizados.Any(o => o.Equals(this.Cbo.CodCbo));
             }
         }
 
